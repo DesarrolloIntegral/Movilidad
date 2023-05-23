4cce992a830b462b4d9b0a7bbaed8321ebf8e90e
Davicho <marin32@hotmail.com>
2023-05-23 11:14:12 -0600

    Commit Architecture

---
+++ DesarrolloIntegral/DesarrolloIntegral.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DesarrolloIntegral.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
